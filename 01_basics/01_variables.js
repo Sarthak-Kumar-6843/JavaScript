fd3270c30658ea3009f8608e4e94443a070a86f5
Sarthak-kumar-6843 <sarthakkumar.k6843@gmail.com>
2025-10-13 23:45:38 +0530

    update

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,11 @@
 const accountId = 1234566
 let accountEmail = "abc@gmail.com"
 var accountPassword = "1234"
-
+accountCity = "delhi"
 // accountId = 2
 
 console.log(accountId);
+
+accountEmail = "adad@gmail.com"
+accountPassword = "1313131"
+accountCity = "jaipur"
