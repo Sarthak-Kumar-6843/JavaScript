4426537dd65e9320bdef32c65ff0783a8ad2d327
Sarthak-kumar-6843 <sarthakkumar.k6843@gmail.com>
2025-10-14 22:43:15 +0530

    data type summary

---
+++ datatypes_summary.js
@@ -0,0 +1,21 @@
+//Primitive
+//7 types : string, number,boolean , null , undefined, Symbol, BigInt
+
+
+//refernece (non primitive)
+//array, objects, functions
+
+//arrays objects function
+
+const hero = ["superman", "batman", "antman"]
+
+let myObj ={
+  name : "Sarthak",
+  age : "19"
+}
+
+const myFunction = function(){
+  console.log("hlo");
+}
+
+console.log(typeof myFunction);
