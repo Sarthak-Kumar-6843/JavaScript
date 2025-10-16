7cb1309d9b7cad2d4c1b4c792fce02c77f8575c6
Sarthak-kumar-6843 <sarthakkumar.k6843@gmail.com>
2025-10-17 00:30:04 +0530

    update

--- 03 basics/02_scopes.js
+++ 03 basics/02_scopes.js
@@ -1,11 +1,52 @@
 if (true) {
   let a = 10
   const b = 20
-  console.log("inner : ",a);
+  // console.log("inner : ",a);
 }
 let a = 300
 // var c = 300
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one() {
+  const username = "sarthak"
+
+  function two(){
+    const website = "youtube"
+    // console.log(username);
+  }
+  // console.log(website);
+
+  two()
+}
+
+// one()
+
+if(true) {
+  const username = "sarthak"
+  if(username === "sarthak"){
+    const website = "  youtube"
+    // console.log(website + username);
+  }
+  // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++ interesting +++++++++++++++++++++++++++
+
+function add1 (n) {
+  return n + 1;
+}
+
+add1 (5)
+
+const add2 = function(n) {
+  return n + 2;
+}
+
+add2(5)
